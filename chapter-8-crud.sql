0288bde39d79f0248325aa1c88dea2f7b2764da1
Alex W. de Freitas <otaru1921@gmail.com>
2021-06-04 18:00:59 -0300

    Introduction to SQL - Chapter 8 - Indentação do Igor
    Fixing some text.

--- chapter-8-crud.sql
+++ chapter-8-crud.sql
@@ -1,54 +1,33 @@
--- select database
-use contacts;
+-- USE database
+USE contacts;
 
--- select * from person
+-- SELECT * FROM person
 SELECT		*
 FROM		person;
 
 -- DELETE Example
-DELETE FROM		person 
-WHERE			person.person_id	= 5;
+DELETE FROM	person 
+WHERE		person.person_id	= 5;
 
 -- INSERT Example [ Not sure about indentation. ]
-INSERT INTO person	(
-					person_id,	
-					person_first_name,	
-					person_last_name,	
-					person_contacted_number,	
-					person_date_last_contacted,		
-					person_date_added)
-			VALUES	(5,			
-					'Johnny',				
-					'Cage',			
-					0,							
-					'2016-05-14 11:43:32',							
-					'2016-05-14 11:43:31')
-					,
-					(6,			
-					'Liu',				
-					'Kang',			
-					0,							
-					'2016-05-14 11:43:32',							
-					'2016-05-14 11:43:31')
-					,
-					(7,			
-					'Sonia',				
-					'Blade',			
-					0,							
-					'2016-05-14 11:43:32',							
-					'2016-05-14 11:43:31')
-					,
-					(42,			
-					'Scorpion',				
-					'Subzero',			
-					0,							
-					'2016-05-14 11:43:32',							
-					'2016-05-14 11:43:31');
+INSERT INTO person	(person_id,	person_first_name, person_last_name, person_contacted_number, person_date_last_contacted, person_date_added)
+			VALUES	(5,	'Johnny', 'Cage', 0, '2016-05-14 11:43:32',	'2016-05-14 11:43:31'),
+					(6,	'Liu',	'Kang',	0,	'2016-05-14 11:43:32',	'2016-05-14 11:43:31'),
+					(7,	'Sonia', 'Blade', 0, '2016-05-14 11:43:32',	'2016-05-14 11:43:31'),
+					(42, 'Scorpion', 'Subzero',	0, '2016-05-14 11:43:32', '2016-05-14 11:43:31');
+
+-- BULK INSERT from SELECT Query [ MySQL ]
+/*
+INSERT INTO        person P
+SELECT             *
+FROM               person OP
+WHERE              OP.person_id > 300;
+*/
 
 
--- Indentação para INSERT [?]
+-- Indenting for INSERT [?]
 
--- Versão 1
+-- Version 1
 INSERT INTO person	(
 					person_id,	
 					person_first_name,	
@@ -63,7 +42,7 @@ INSERT INTO person	(
 					NULL,							
 					'2016-05-14 11:43:31');
 
--- Versão 2
+-- Version 2
 INSERT INTO person	(
 				person_id,	
 				person_first_name,	
@@ -78,20 +57,20 @@ INSERT INTO person	(
 				NULL,							
 				'2016-05-14 11:43:31');
 
--- Versão 3 - Preferida do Igor
+-- Version 3 - Igor uses this version.
 INSERT INTO person	(person_id,	person_first_name, person_last_name, person_contacted_number, person_date_last_contacted, person_date_added)
 			VALUES	(1,	'Jon', 'Flanders',	0, NULL, '2016-05-14 11:43:31');
 
 
--- Versão 4
+-- Version 4
 INSERT INTO person	(person_id,		person_first_name,		person_last_name,		person_contacted_number,	person_date_last_contacted,		person_date_added		)
 			VALUES	(1,				'Jon',					'Flanders',				0,							NULL,							'2016-05-14 11:43:31'	);
 
--- Versão 5
+-- Version 5
 INSERT INTO person	(person_id	,	person_first_name	,	person_last_name	,	person_contacted_number	,	person_date_last_contacted	,	person_date_added		)
 			VALUES	(1			,	'Jon'				,	'Flanders'			,	0						,	NULL						,	'2016-05-14 11:43:31'	);
 
--- Versão 6
+-- Version 6
 INSERT INTO person	(person_id,		person_first_name,		person_last_name,		person_contacted_number,	person_date_last_contacted,		person_date_added		)
 			VALUES	(        1,		            'Jon',			  'Flanders',							  0,						  NULL,		'2016-05-14 11:43:31'	);
 					
