3f863828732d21072c851071eeea49fcb3947826
Alex W. de Freitas <otaru1921@gmail.com>
2021-06-04 15:48:54 -0300

    Introduction to SQL - Chapter 6 - Completed Chapter 6
    - ORDER BY
    - GROUP BY
    - AVG, SUM, MAX, COUNT, MIN
    - HAVING

--- chapter-6/chapter-6-order-by-group-by.sql
+++ chapter-6/chapter-6-order-by-group-by.sql
@@ -7,7 +7,7 @@ SELECT		P.person_first_name		as	FirstName,
 FROM		person						P
 ORDER BY	P.person_last_name;
 
--- What is the total number of times I've contacted my contacts?
+-- What is the total number of times I've contacted someone in my contacts?
 SELECT	SUM(P.person_contacted_number)		as	ContactedTotal
 FROM	Person									P;
 
@@ -18,14 +18,29 @@ WHERE	P.person_last_name				=	'Ahern';
 
 -- What is the maximum number of times I have contacted a person?
 SELECT	MAX(P.person_contacted_number)		as	MaxContacted
-FROM	Person								P;
+FROM	Person									P;
 
 -- What is the minimum number of times I have contacted a person?
 SELECT	MIN(P.person_contacted_number)		as	MinContacted
-FROM	Person								P;
+FROM	Person									P;
 
 -- What is the average number of times I have contacted a person?
 SELECT	AVG(P.person_contacted_number)		as	AverageContacted
-FROM	Person								P;
+FROM	Person									P;
 
 -- What is the count of unique first names among my contacts?
+SELECT	COUNT(DISTINCT P.person_first_name)		as	'Unique First Names In Contact List'
+FROM	Person										P;
+
+-- What is the count of every unique first name among my contacts?
+SELECT		COUNT(P.person_first_name)				as	NameCount, 
+			P.person_first_name						as	FirstName
+FROM		Person										P
+GROUP BY	P.person_first_name;
+
+-- What is the count of unique first names among my contacts that appear at least once?
+SELECT		COUNT(P.person_first_name)		as	NameCount,
+			P.person_first_name				as	FirstName
+FROM		Person								P
+GROUP BY	P.person_first_name
+HAVING		COUNT(P.person_first_name)		>=	1;
