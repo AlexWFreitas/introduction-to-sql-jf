24edcf85fc50cf489eff6cebb744a9df8a9dc6e9
Alex Wong de Freitas <otaru1921@gmail.com>
2021-06-07 11:55:27 -0300

    Introduction to SQL - Chapter 8 - UPDATE and DELETE

--- chapter-8-crud.sql
+++ chapter-8-crud.sql
@@ -1,28 +1,32 @@
 -- USE database
-USE contacts;
+USE Contacts;
 
 -- SELECT * FROM person
 SELECT		*
 FROM		person;
 
+-- SELECT * FROM email_address
+SELECT		*
+FROM		email_address;
+
 -- DELETE Example
 DELETE FROM	person 
 WHERE		person.person_id	= 5;
 
--- INSERT Example [ Not sure about indentation. ]
+-- INSERT Example
 INSERT INTO person	(person_id,	person_first_name, person_last_name, person_contacted_number, person_date_last_contacted, person_date_added)
 			VALUES	(5,	'Johnny', 'Cage', 0, '2016-05-14 11:43:32',	'2016-05-14 11:43:31'),
 					(6,	'Liu',	'Kang',	0,	'2016-05-14 11:43:32',	'2016-05-14 11:43:31'),
 					(7,	'Sonia', 'Blade', 0, '2016-05-14 11:43:32',	'2016-05-14 11:43:31'),
 					(42, 'Scorpion', 'Subzero',	0, '2016-05-14 11:43:32', '2016-05-14 11:43:31');
 
--- BULK INSERT from SELECT Query [ MySQL ]
-/*
-INSERT INTO        person P
+-- BULK INSERT from SELECT Query
+
+INSERT INTO        person
 SELECT             *
 FROM               person OP
 WHERE              OP.person_id > 300;
-*/
+
 
 
 -- Indenting for INSERT [?]
@@ -73,3 +77,25 @@ INSERT INTO person	(person_id	,	person_first_name	,	person_last_name	,	person_co
 INSERT INTO person	(person_id,		person_first_name,		person_last_name,		person_contacted_number,	person_date_last_contacted,		person_date_added		)
 			VALUES	(        1,		            'Jon',			  'Flanders',							  0,						  NULL,		'2016-05-14 11:43:31'	);
 					
+-- UPDATE - Change email address to something on rows where email_address_id is 5.
+UPDATE		E
+SET			E.email_address = 'aaron@mail.com'
+FROM		email_address E
+WHERE		E.email_address_id = 5;
+
+-- UPDATE Example 2 - Updating multiple fields
+UPDATE		P
+SET			P.person_first_name = 'Bob',
+			P.person_last_name = 'Foo'
+FROM		person P
+WHERE		P.person_id = 1;
+
+
+-- BAD PRACTICE - DELETES all rows from table person.
+-- DELETE FROM		person;
+
+
+-- DELETE - Example Query
+DELETE FROM		person
+FROM			person P
+WHERE			P.person_id > 4;
