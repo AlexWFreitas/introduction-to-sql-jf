b956825ffb6065c2bffe9649ba3dee88761b6d18
Alex Wong de Freitas <otaru1921@gmail.com>
2021-06-07 10:59:20 -0300

    Introduction to SQL - Chapter 8 - Teste de Commit Pós-Format

--- chapter-8-crud.sql
+++ chapter-8-crud.sql
@@ -61,7 +61,6 @@ INSERT INTO person	(
 INSERT INTO person	(person_id,	person_first_name, person_last_name, person_contacted_number, person_date_last_contacted, person_date_added)
 			VALUES	(1,	'Jon', 'Flanders',	0, NULL, '2016-05-14 11:43:31');
 
-
 -- Version 4
 INSERT INTO person	(person_id,		person_first_name,		person_last_name,		person_contacted_number,	person_date_last_contacted,		person_date_added		)
 			VALUES	(1,				'Jon',					'Flanders',				0,							NULL,							'2016-05-14 11:43:31'	);
